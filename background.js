1b5718e07060c840e87f5fb204feb50402ea40b4
przemelek <przemelek@gmail.com>
2014-01-25 15:59:36 +0100

    Update to 0.0.3, fixes
    Fix of problem when we had network problem during any of operations. To
    this moment extension simple stoped to work, and started to work again
    after browser restart.
    Additionaly instead of building strings with JSON objects to send to
    Pocket, objects are build in JavaScript, and next stringified.

--- background.js
+++ background.js
@@ -13,20 +13,25 @@ var oauth = new OAuth({
 // function fetch(url,method,headers,body,callback,async,returnAsXML)
 function retrieve(since) {
 	var url = "https://getpocket.com/v3/get";
-	var body = '{"consumer_key":"'+consumer_key+'","access_token":"'+oauth.getAuthToken()+'","contentType":"article","sort":"oldest"';
+	var bodyObj = {"consumer_key":consumer_key,"access_token":oauth.getAuthToken(),"contentType":"article","sort":"oldest"};	
 	if (since) {
-		body+=',"since":"'+since+'"';
+		bodyObj.since=since;
 	}
-	body+="}";
+	var body = JSON.stringify(bodyObj);
 	var headers = [];	 
-	headers.push({"name":"Content-Type","value":"application/json; charset=UTF8"});	
-	var response = fetch(url,"POST",headers,body,null,false,false);
-	//console.log(response);
-	return response;
+	headers.push({"name":"Content-Type","value":"application/json; charset=UTF8"});
+	try {
+		var response = fetch(url,"POST",headers,body,null,false,false);
+		return response;
+	} catch (e) {
+		// OK, something went wrong, proceed
+		return '{"list":{}}';
+	}
 }
 
 function modify(articles) {
-	var body =  '{"consumer_key":"'+consumer_key+'","access_token":"'+oauth.getAuthToken()+'","actions":[';
+	var bodyObj = {"consumer_key":consumer_key,"access_token":oauth.getAuthToken()};
+	bodyObj.actions=new Array();
     for (var i=0; i<articles.length; i++) {
 		var article = articles[i];
 		var minutesToRead = Math.floor(article.word_count/200);
@@ -41,24 +46,30 @@ function modify(articles) {
 		else tag = "30+ minutes";
 		var tags = ["1 minute or less","2 minutes or less","5 minutes or less","10 minutes or less","15 minutes or less","30 minutes or less","30+ minutes"].filter(function(x) { return x!=tag; });		
 		var url = "https://getpocket.com/v3/send";
-		body +=  '{"action":"tags_remove","tags":'+JSON.stringify(tags)+',"item_id":"'+itemId+'"},';
-		body +=  '{"action":"tags_add","item_id":"'+itemId+'","tags":["'+tag+'"]},';					
+		var removeTagsAction = {"action":"tags_remove","tags":JSON.stringify(tags),"item_id":itemId};
+		var addTagAction = {"action":"tags_add","tags":new Array(tag),"item_id":itemId};
+		bodyObj.actions.push(removeTagsAction);
+		bodyObj.actions.push(addTagAction);
 	}
-	body=body.substring(0,body.length-1);
-	body+=']}';
+	var body = JSON.stringify(bodyObj);
 	var headers = [];
 	headers.push({"name":"Content-Type","value":"application/json; charset=UTF8"});	
-	var response = fetch(url,"POST",headers,body,null,false,false);
-	//console.log(response);
+	try {
+		var response = fetch(url,"POST",headers,body,null,false,false);
+		// OK, here we should pass value calculated from response....
+		return true;
+	} catch(e) {
+		return false;
+	}
 }
 
 function addTagsToAllNewPosts() {
 	var list = JSON.parse(retrieve(localStorage[sinceKey]));
-	//console.log(list.length);
-	//console.log(list.list);
 	var count = 0;
 	var wordsCount = 0;
 	var articles = [];
+	var canUpdateSince = true;
+	var needToUpdateSince = false;
 	for (var key in list.list) {
 		var article = list.list[key];
 		if (article.status!="0") {
@@ -66,20 +77,28 @@ function addTagsToAllNewPosts() {
 			continue;
 		}
 		count++;
+		wordsCount+=article.word_count*1;
 		articles.push(article);
 		if (articles.length==100) {
-			modify(articles);
+			canUpdateSince&=modify(articles);
+			needToUpdateSince = true;
 			articles = [];
 		}
 	}
 	if (articles.length!=0) {
-		modify(articles);
+		canUpdateSince&=modify(articles);
+		needToUpdateSince = true;
 	}
-	// OK, may make problems...
-	var list2 = JSON.parse(retrieve(Math.floor(new Date().getTime()/1000)));
-	localStorage[sinceKey]=list2.since;
+	if (needToUpdateSince && canUpdateSince) {
+		// OK, may make problems...	
+		var list2 = JSON.parse(retrieve(Math.floor(new Date().getTime()/1000)));
+		if (list2.since) {
+			localStorage[sinceKey]=list2.since;
+		}
+	}
+	
 	console.log("count="+count);
-	//console.log("wordsCount="+wordsCount);
+	console.log("wordsCount="+wordsCount);
 	setTimeout(addTagsToAllNewPosts,5*60*1000);
 }
 
